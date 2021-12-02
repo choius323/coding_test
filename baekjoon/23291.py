6a85db7ca603a3fadb1200bc5264f2803bb932d1
choius323 <chldn323@gmail.com>
2021-12-02 12:36:29 +0900

    update baekjoon 23291

--- baekjoon/23291.py
+++ baekjoon/23291.py
@@ -1,59 +1,42 @@
 """
+어항 문제
 
-8 7
-5 2 3 14 9 2 11 8
 
-TypeError 뜸
 
 """
 
-
-def leviation90(index):
+# 90도 회전 및 공중부양
+def leviation90(index: int):
     # topIndex = max(row for row in range(len(fishes)) if fishes[row][0] is not None)
-    global topIndex
+    topIndex = len(fishes)
+    [
+        fishes.append([])
+        for _ in range(index - len(fishes) + 1)
+        if index + 1 > len(fishes)
+    ]
     for row in range(topIndex):
         for col in range(index):
-            fishes[index - col][index + row] = fishes[row][col]
-            fishes[row][col] = None
-
-    for row in range(index + 1):
-        for col in range(index):
-            fishes[row].append(fishes[row].pop(0))
-    topIndex = index + 1
+            fishes[index - col] += [fishes[row].pop(0)]
 
 
+# 180도 회전 및 공중부양
 def levitation180():
-    index = rightIndex
-    global topIndex
+    topIndex = len(fishes)
+    index = len(fishes[0]) // 2
+    [fishes.append([]) for _ in range(topIndex)]
     for row in range(topIndex):
-        for col in range(index):
-            fishes[2 * topIndex - row - 1][2 * index - col - 1] = fishes[row][col]
+        for col in range(index - 1, -1, -1):
+            # fishes[2 * topIndex - row - 1][2 * index - col - 1] = fishes[row][col]
             # index - row + index - 1 => 2 * index - row - 1
-            fishes[row][col] = None
-
-    for row in range(topIndex * 2):
-        for col in range(index):
-            fishes[row].append(fishes[row].pop(0))
-    topIndex = topIndex * 2
+            fishes[2 * topIndex - row - 1] += [fishes[row].pop(col)]
 
 
+# 물고기 분배
 def divide():
-    global topIndex
-    global rightIndex
+    fishesModi = [[0] * len(temp) for temp in fishes]
 
-    fishesModi = list()
-    for row in range(topIndex):
-        countNone = 0
-        for value in fishes[row]:
-            if value is not None:
-                countNone += 1
-            else:
-                break
-        fishesModi.append([0] * countNone)
-    # fishesModi.pop()
-
-    for row in range(topIndex):
-        for col in range(len(fishesModi[row])):
+    for row in range(len(fishes)):
+        for col in range(len(fishes[row])):
             if col > 0:
                 fishesModi[row][col] += addFishes(
                     fishes[row][col], fishes[row][col - 1]
@@ -66,82 +49,70 @@ def divide():
                 fishesModi[row][col] += addFishes(
                     fishes[row][col], fishes[row - 1][col]
                 )
-            if row < topIndex - 1:
+            if row < len(fishes) - 1 and len(fishes[row + 1]) > col:
                 fishesModi[row][col] += addFishes(
                     fishes[row][col], fishes[row + 1][col]
                 )
 
-    for row in range(topIndex):
-        for col in range(len(fishesModi[row])):
+    for row in range(len(fishes)):
+        for col in range(len(fishes[row])):
             fishes[row][col] += fishesModi[row][col]
+    toFloor()
 
     # print(fishesModi)
+    # print(fishes)
 
 
+# 계산
 def addFishes(a, b):
-    if b is not None:
-        if a >= b:
-            return -((a - b) // 5)
-        elif b > a:
-            return (b - a) // 5
-    else:
-        return 0
+    if a >= b:
+        return -((a - b) // 5)
+    elif b > a:
+        return (b - a) // 5
 
 
-def toFloor(sliceIndex):
-    global topIndex
+# 바닥에 재정렬
+def toFloor():
+    sliceIndex = len(fishes[len(fishes) - 1])
     index = 1
     for _ in range(sliceIndex):
-        for row in range(1, topIndex):
+        for row in range(1, len(fishes)):
             fishes[0].insert(index, fishes[row].pop(0))
-            fishes[0].pop()
-            fishes[row].append(None)
             index += 1
         index += 1
-    topIndex = 1
+    [fishes.pop() for i in range(len(fishes) - 1)]
+    # print(fishes)
 
 
+# 값 입력
 N, K = map(int, input().split())
-fishes = [[None] * N for _ in range(N)]
-fishes[0] = list(map(int, input().split()))
+fishes = [list(map(int, input().split()))]
 
 
 count = 0
 while True:
-    minFish = min(value for value in fishes[0] if value is not None)
+    minFish = min(fishes[0])
     while fishes[0].count(minFish) > 0:
         fishes[0][fishes[0].index(minFish)] = minFish + 1
     # print(fishes)
-    topIndex = 1
-    rightIndex = N
 
     n = 2
-    while topIndex <= rightIndex - n // 2:
+    while len(fishes) <= len(fishes[0]) - n // 2:
         leviation90(n // 2)
-        rightIndex -= n // 2
         n += 1
         # print(fishes)
 
     divide()
-    # print(fishes)
-    toFloor(n // 2)
-    # print(fishes)
 
-    maxF = max(fishes[0])
-    minF = min(fishes[0])
-    rightIndex = N // 2
     count += 1
     for _ in range(2):
         levitation180()
-        rightIndex //= 2
         # print(fishes)
     divide()
-    # print(fishes)
-    toFloor(n + 1)
-    # print(fishes)
-    maxF = max(fishes[0])
-    minF = min(fishes[0])
-    if maxF - minF <= K:
+
+    maxFish = max(fishes[0])
+    minFish = min(fishes[0])
+    if maxFish - minFish <= K:
         break
 
 # print(fishes)
