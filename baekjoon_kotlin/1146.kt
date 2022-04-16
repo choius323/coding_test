32012f78c6b61736aac97993167c6f738728a89e
CUS <chldn323@gmail.com>
2022-04-16 22:22:42 +0900

    Update

---
+++ baekjoon_kotlin/1146.kt
@@ -0,0 +1,67 @@
+/*
+
+1146번 - RGB거리
+https://www.acmicpc.net/problem/1146
+
+계단 오르기 문제처럼 생각했었지만 복잡하기만 하고 필요하지 않았다.
+색이 3가지 밖에 없기 때문에 집마다 경우의 수는 총 6개(현재 색상 3, 직전 집 색상 2)이므로 직접 계산해도 충분하다.
+문제를 보는 시각을 또 넓힐 수 있었고, 여러 시도를 하면서 코틀린에 대한 이해도가 많이 늘었다.
+
+*/
+
+package baekjoon_kotlin
+
+fun main() = java.io.StreamTokenizer(System.`in`.bufferedReader()).run {
+    fun i() = (nextToken()).run { nval.toInt() }
+    val n = i()
+    val answer = IntArray(3)
+    val choice = arrayOf(intArrayOf(1, 2), intArrayOf(0, 2), intArrayOf(0, 1))
+    var last = IntArray(3)
+    repeat(n) {
+        for (i in 0..2) {
+            answer[i] = last[choice[i][0]].coerceAtMost(last[choice[i][1]]) + i()
+        }
+        last = answer.clone()
+    }
+    print(answer.minOrNull())
+}
+
+//fun main() = (System.`in`.bufferedReader()).run {
+//    fun IntArray.min(a: Int = 1001, b: Int = 1001): Pair<Int, Int> {
+//        val newThis = this.filterIndexed { index, _ -> index != a && index != b }
+//        val min = newThis.minOrNull()!!
+//        var minIndex = newThis.indexOf(min)
+//        if (minIndex >= a) minIndex += 1
+//        if (minIndex >= b) minIndex += 1
+//        return min to minIndex
+//    }
+//
+//    operator fun Pair<Int, Int>.plus(int: Int) = this.first + int to this.second
+//
+//    val n = readLine().toInt()
+//    val prices = Array(n) { readLine().split(" ").map { it.toInt() }.toIntArray() }
+//    val answer = Array(n) { 0 to 0 }  // 가격 합, 색(0,1,2)
+//    answer[0] = prices[0].min()
+//    if (n > 1) {
+//        answer[1] = prices[1].min(answer[0].second)
+//        answer[1] = answer[1] + answer[0].first
+//    }
+//
+//    for (i in 2 until n) {
+//        answer[i] = prices[i].min() + answer[i - 2].first
+//        answer[i] = answer[i] + prices[i - 1].min(answer[i].second, answer[i - 2].second).first
+//        val comparePair = prices[i].min(answer[i - 1].second) + answer[i - 1].first
+//        println("${answer[i]} $comparePair")
+//        if (answer[i].first > comparePair.first) answer[i] = comparePair
+//    }
+//    print(answer.contentDeepToString())
+//}
+
+/*
+3
+1 3 9
+1 2 9
+3 1 9
+
+5
+*/
---
+++ baekjoon_kotlin/1753.kt
@@ -0,0 +1,120 @@
+/*
+
+1753번 - 최단경로
+https://www.acmicpc.net/problem/1753
+
+인접 리스트와 우선순위 큐를 사용해서 다익스트라 알고리즘을 구현했다.
+인접 행렬을 사용하면 V^2 크기의 배열이 필요한데, 여기선 최대 20000이기 때문에 메모리 초과가 발생해서 인접 리스트를 사용해야 한다.
+TreeMap은 Key를 기준으로 정렬하는 기능밖에 없기 때문에 Value를 활용하기 위해 우선순위 큐를 사용했다.
+
+*/
+
+package baekjoon_kotlin
+
+import java.io.StreamTokenizer
+import java.util.*
+import kotlin.math.min
+
+fun main() = StreamTokenizer(System.`in`.bufferedReader()).run {
+    fun i() = nextToken().run { nval.toInt() }
+    val V = i()  // 정점의 개수
+    val E = i()  // 간선의 개수
+    val K = i()  // 시작 정점
+    val graph = Array(V + 1) { HashMap<Int, Int>() }  // 간선 - 인접 리스트 구현. graph[시작점][중간지점] -> 거리
+    val answer = IntArray(V + 1) { Int.MAX_VALUE }  // 최단 거리
+    answer[K] = 0
+    repeat(E) {
+        val u = i()
+        val v = i()
+        val w = i()
+        graph[u][v] = min(graph[u][v] ?: Int.MAX_VALUE, w)  // 중복으로 들어올 때 더 작은 값만 저장
+    }
+    val queue = PriorityQueue<Pair<Int, Int>> { o1, o2 -> o1.second - o2.second }
+    queue.add(K to 0)
+
+    while (queue.isNotEmpty()) {
+        val (mid, dist) = queue.poll()
+        for (arrive in graph[mid].keys) {
+            val nValue = dist + graph[mid][arrive]!!
+            if (nValue < answer[arrive]) {
+                queue += arrive to nValue
+                answer[arrive] = nValue
+            }
+        }
+    }
+
+    val sb = StringBuilder()
+    for (i in 1..V) {
+        if (answer[i] == Int.MAX_VALUE) sb.append("INF\n")
+        else sb.appendLine(answer[i])
+    }
+    print(sb)
+}
+
+/*
+5 6
+1
+5 1 1
+1 2 2
+1 3 3
+2 3 4
+2 4 5
+3 4 6
+
+0
+2
+3
+7
+INF
+
+
+5 9
+1
+5 1 1
+1 2 5
+1 3 5
+2 3 3
+2 4 1
+3 4 6
+3 2 7
+1 2 2
+4 3 1
+
+0
+2
+4
+3
+INF
+
+
+4 5
+1
+1 3 2
+3 2 4
+1 4 2
+4 2 1
+1 3 5
+
+0
+3
+2
+2
+
+
+6 6
+1
+1 2 1
+2 5 1
+5 6 1
+1 3 10
+3 4 5
+6 3 1
+
+0
+1
+4
+9
+2
+3
+
+*/
