658ce34c11fe07c4cf0b5cf8984501143e2d0263
ChoiUS <chldn323@gmail.com>
2023-08-21 00:00:35 +0900

    문제 풀이 업로드

--- baekjoon_kotlin/10926.kt
+++
@@ -1,5 +0,0 @@
-package baekjoon_kotlin
-
-import java.util.Scanner as s
-
-fun main() = print(s(System.`in`).next()+ "??!")
--- baekjoon_kotlin/_10000/10926.kt
+++ baekjoon_kotlin/_10000/10926.kt
@@ -1,3 +1,17 @@
 package baekjoon_kotlin._10000
 
+/*
+
+10926번 - ??!
+https://www.acmicpc.net/problem/10926
+분류 : 구현
+
+입력받은 문자열에 "??!"를 더햇 출력한다.
+
+*/
+
 fun main()=print(readln()+"??!")
+
+/*
+
+*/
